7d91dc4d8e5d5db007286df1522ba7ce50f47b58
NattaveeWilainumchokechai <92027360+NattaveeWilainumchokechai@users.noreply.github.com>
2022-10-11 14:38:12 +0700

    Add files via upload

---
+++ select.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <?php 
+        if(empty($_GET)){
+            ?>ใส่ค่า language<?php
+        }
+        else{
+            if(!isset($_COOKIE["lang"])){
+                setcookie("lang",$_GET["language"],time()+3600*24);
+                ?>สร้าง cookie lang แล้ว<?php
+            }else{
+                setcookie("lang",$_GET["language"],time()+3600*24);
+                ?>setค่า cookie lang ใหม่แล้ว<?php
+            }
+        }         
+        ?>
+</body>
+</html>
